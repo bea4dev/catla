4433bfa7bdf2aa96b16870f71786deb08941cb49
bea4dev <bea0224@outlook.jp>
2025-01-12 05:08:18 +0900

    commit

--- catla_transpile_std/src/holder.rs
+++ catla_transpile_std/src/holder.rs
@@ -6,29 +6,44 @@ pub struct CatlaObjectHolder<T: CatlaRefManagement + Copy> {
 }
 
 impl<T: CatlaRefManagement + Copy> CatlaObjectHolder<T> {
-    pub fn new(object: T) -> Self {
-        Self {
-            object,
-        }
-    }
-
+    #[inline(always)]
     pub fn clone(&self) -> T {
         self.object.clone_ref();
         self.object
     }
 
+    #[inline(always)]
     pub fn clone_non_mutex(&self) -> T {
         self.object.clone_ref_non_mutex();
         self.object
     }
 
-    pub fn last_use(self) -> T {
+    #[inline(always)]
+    pub fn consume(self) -> T {
+        self.object
+    }
+
+    #[inline(always)]
+    pub fn borrow(&self) -> T {
         self.object
     }
 }
 
 impl<T: CatlaRefManagement + Copy> Drop for CatlaObjectHolder<T> {
+    #[inline(always)]
     fn drop(&mut self) {
         self.object.drop_ref();
+    }
+}
+
+
+pub trait Hold where Self: CatlaRefManagement + Copy {
+    fn hold(self) -> CatlaObjectHolder<Self>;
+}
+
+impl<T: CatlaRefManagement + Copy> Hold for T {
+    #[inline(always)]
+    fn hold(self) -> CatlaObjectHolder<Self> {
+        CatlaObjectHolder { object: self }
     }
 }
